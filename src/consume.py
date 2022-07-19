aa9e5f1963880f3d18b73851f4823bfffb1d5dc3
Eduardo Pagotto <eduardo.pagotto@newspace.com.br>
2022-07-19 16:27:40 -0300

    Producer e  Consumer Funcionais no basico

--- src/consume.py
+++ src/consume.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python3
 '''
 Created on 20220710
-Update on 20220718
+Update on 20220719
 @author: Eduardo Pagotto
  '''
 
-from pulsar import Client, schema
+from pulsar import Client
+from pulsar import schema as sc
 
+class RpaTeste(sc.Record):
+    nome = sc.String()
+    idade = sc.Integer()
 
 def main():
     client = Client('pulsar://localhost:6650')
-    consumer = client.subscribe('my-topic',subscription_name='my-sub', schema=schema.JsonSchema())
+    consumer = client.subscribe('persistent://rpa/ns01/tp01',subscription_name='my-sub', schema=sc.JsonSchema(RpaTeste))
 
     while True:
         msg = consumer.receive()
--- src/producer.py
+++ src/producer.py
@@ -1,36 +1,30 @@
 #!/usr/bin/env python3
 '''
 Created on 20220710
-Update on 20220711
+Update on 20220719
 @author: Eduardo Pagotto
  '''
 
-#import json
 from pulsar import Client
 from pulsar import schema as sc
 
-class rpaTeste(sc.Record):
+class RpaTeste(sc.Record):
     nome = sc.String()
     idade = sc.Integer()
 
 def main():
 
-    # schema : dict = {}
-    # with open('./pulsar-dsk/schema2.json', 'r') as file:
-    #     schema = json.load(file)
-
     try:
         client = Client('pulsar://localhost:6650')
-        producer = client.create_producer('persistent://rpa/ns01/tp01', schema=sc.JsonSchema(schema))
+        producer = client.create_producer('persistent://rpa/ns01/tp01', schema=sc.JsonSchema(RpaTeste))
 
-        t = rpaTeste
+        t = RpaTeste()
         t.nome = "edu"
 
         for i in range(10):
             t.idade = i
             producer.send(t)
-            #producer.send(('hello-pulsar-%d' % i).encode('utf-8'))
-
+           
     except Exception as exp:
         print(str(exp))
     finally:
